53ec6e7e2b8aade0692729806df518e059fdad45
Pedro Henrique <pedrohalmeidamendonca@gmail.com>
2024-03-01 17:45:52 -0300

    Integrating shadcn-ui, convex and clerk

---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://innocent-chicken-19.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
---
+++ providers/convex-client-provider.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { ClerkProvider, useAuth } from "@clerk/nextjs";
+import { AuthLoading, Authenticated, ConvexReactClient } from "convex/react";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+
+import { Loading } from "@/components/auth/loading";
+
+interface ConvexClientProviderProps {
+  children: React.ReactNode;
+}
+
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+
+export const ConvexClientProvider = ({
+  children,
+}: ConvexClientProviderProps) => {
+  return (
+    <ClerkProvider>
+      <ConvexProviderWithClerk useAuth={useAuth} client={convex}>
+        <Authenticated>{children}</Authenticated>
+        <AuthLoading>
+          <Loading />
+        </AuthLoading>
+      </ConvexProviderWithClerk>
+    </ClerkProvider>
+  );
+};
