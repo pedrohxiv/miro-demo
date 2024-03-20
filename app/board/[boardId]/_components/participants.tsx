788e2edb6c0ad9b309cd5a337f6c97a9a44d447d
Pedro Henrique <pedrohalmeidamendonca@gmail.com>
2024-03-20 16:19:25 -0300

    Creating room participants

--- app/board/[boardId]/_components/participants.tsx
+++ app/board/[boardId]/_components/participants.tsx
@@ -1,6 +1,44 @@
+"use client";
+
+import { connectionIdToColor } from "@/lib/utils";
+import { useOthers, useSelf } from "@/liveblocks.config";
+
+import { UserAvatar } from "./user-avatar";
+
+const MAX_SHOW_OTHERS = 2;
+
 export const Participants = () => {
+  const others = useOthers();
+  const self = useSelf();
+
   return (
-    <div className="absolute h-12 top-2 right-2 bg-white rounded-md p-3 flex items-center shadow-md"></div>
+    <div className="absolute h-12 top-2 right-2 bg-white rounded-md p-3 flex items-center shadow-md">
+      <div className="flex gap-x-2">
+        {others.slice(0, MAX_SHOW_OTHERS).map((other) => (
+          <UserAvatar
+            key={other.connectionId}
+            src={other.info?.picture}
+            name={other.info?.name}
+            fallback={other.info?.name?.[0] || "A"}
+            borderColor={connectionIdToColor(other.connectionId)}
+          />
+        ))}
+        {self && (
+          <UserAvatar
+            src={self.info?.picture}
+            name={`${self.info?.name} (You)`}
+            fallback={self.info?.name?.[0]}
+            borderColor={connectionIdToColor(self.connectionId)}
+          />
+        )}
+        {others.length > MAX_SHOW_OTHERS && (
+          <UserAvatar
+            name={`${others.length - MAX_SHOW_OTHERS} more`}
+            fallback={`+${others.length - MAX_SHOW_OTHERS}`}
+          />
+        )}
+      </div>
+    </div>
   );
 };
 
--- lib/utils.ts
+++ lib/utils.ts
@@ -2,3 +2,9 @@ import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export const cn = (...inputs: ClassValue[]) => twMerge(clsx(inputs));
+
+export const connectionIdToColor = (connectionId: number) => {
+  const COLORS = ["#DC2626", "#D97706", "#059669", "#7C3AED", "#DB2777"];
+
+  return COLORS[connectionId % COLORS.length];
+};
