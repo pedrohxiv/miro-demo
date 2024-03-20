2f0edfea3ed042bd751c22c265361ab8d2c1b7bf
Pedro Henrique <pedrohalmeidamendonca@gmail.com>
2024-03-20 15:12:42 -0300

    Setuping liveblocks and implemeting room authentication

---
+++ liveblocks.config.ts
@@ -0,0 +1,79 @@
+import { createClient } from "@liveblocks/client";
+import { createLiveblocksContext, createRoomContext } from "@liveblocks/react";
+
+const client = createClient({
+  authEndpoint: "/api/liveblocks-auth",
+});
+
+type Presence = {};
+
+type Storage = {};
+
+type UserMeta = {
+  id?: string;
+  info?: {
+    name?: string;
+    picture?: string;
+  };
+};
+
+type RoomEvent = {};
+
+export type ThreadMetadata = {};
+
+export const {
+  suspense: {
+    RoomProvider,
+    useRoom,
+    useMyPresence,
+    useUpdateMyPresence,
+    useSelf,
+    useOthers,
+    useOthersMapped,
+    useOthersListener,
+    useOthersConnectionIds,
+    useOther,
+    useBroadcastEvent,
+    useEventListener,
+    useErrorListener,
+    useStorage,
+    useObject,
+    useMap,
+    useList,
+    useBatch,
+    useHistory,
+    useUndo,
+    useRedo,
+    useCanUndo,
+    useCanRedo,
+    useMutation,
+    useStatus,
+    useLostConnectionListener,
+    useThreads,
+    useCreateThread,
+    useEditThreadMetadata,
+    useCreateComment,
+    useEditComment,
+    useDeleteComment,
+    useAddReaction,
+    useRemoveReaction,
+    useThreadSubscription,
+    useMarkThreadAsRead,
+    useRoomNotificationSettings,
+    useUpdateRoomNotificationSettings,
+  },
+} = createRoomContext<Presence, Storage, UserMeta, RoomEvent, ThreadMetadata>(
+  client
+);
+
+export const {
+  suspense: {
+    LiveblocksProvider,
+    useMarkInboxNotificationAsRead,
+    useMarkAllInboxNotificationsAsRead,
+    useInboxNotifications,
+    useUnreadInboxNotificationsCount,
+    useUser,
+    useRoomInfo,
+  },
+} = createLiveblocksContext<UserMeta, ThreadMetadata>(client);
